4d9f7e02d8d4af1afa4f9fc5af7bdb86d812946b
lizadad07 <elizabethdadashev@gmail.com>
2024-09-19 19:35:20 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Main {
+	static Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+        BankAccount acctLisa = new BankAccount(1000, -1000, "Lisa");
+        BankAccount acctBob = new BankAccount(1000, -1000, "Bob");
+        
+        System.out.println("Balance Lisa=" + acctLisa.getBalance());
+        System.out.println("Balance Bob=" + acctBob.getBalance());
+        acctLisa.transfer(acctBob, 500); // Lisa transfered to Bob 500
+        System.out.println("Balance Bob after the first transfer =" + acctBob.getBalance());
+        System.out.println("Balance Lisa after the first transfer=" + acctLisa.getBalance());
+        
+        acctBob.transfer(acctLisa,1600); // Bob transfered to Lisa 1600
+        System.out.println("Balance Lisa after the second transfer=" + acctLisa.getBalance());
+        System.out.println("Balance Bob after the second transfer=" + acctBob.getBalance());
+        //System.out.println(acctLisa.toString());
+        
+        // Bob can transform 1600 to lisa, but lisa can't transform 1600 to Bob.
+        //---------------------------------------------------------------------------
+        
+        Rectangle rect1 = new Rectangle(5, 5);
+    	Rectangle rect2 = new Rectangle(5, 15);
+    	//rect1.scale(2);
+    	//rect1.draw();
+//    	int areaOfRect1 = rect1.calcArea();
+//    	int areaOfRect2 = rect2.calcArea();
+    	
+    	//int perimeterOfRect1 = rect1.calcPerimeter();
+    	//int perimeterOfRect2 = rect2.calcPerimeter();
+//    	System.out.println("The sum of the areas of rect1 and rect2: " + (areaOfRect1 + areaOfRect2));
+//    	System.out.println("The sum of the perimeters of rect1 and rect2: " + (perimeterOfRect1 + perimeterOfRect2));
+    	
+        
+    }
+}
